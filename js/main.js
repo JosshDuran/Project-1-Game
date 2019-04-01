260204b711875270e6224ea095da30370d536e00
Jossh Duran <josshduran@Jennifers-MacBook-Pro.local>
2019-04-01 16:08:02 -0700

    update

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 var myPrimary;
+var obstacle;
 
 function startGame() {
   gameArea.start();
   myPrimary = new component(30, 30, "red", 160, 0);
+  obstacle = new component(10, 200, "purple", 300, 120);
 }
 // Create canvas
 var gameArea = {
   canvas : document.createElement("canvas"),
   start : function() {
     this.canvas.width = 480;
-    this.canvas.height = 270;
+    this.canvas.height = 500;
     this.context = this.canvas.getContext("2d");
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
   	this.interval = setInterval(updateGameArea, 20);
@@ -22,6 +24,9 @@ var gameArea = {
 function component(width, height, color, x, y) {
   this.width = width;
   this.height = height;
+
+  this.speedX = 0;
+  this.speedY = 0;
   this.x = x;
   this.y = y;
   this.update = function() {    
@@ -29,12 +34,34 @@ function component(width, height, color, x, y) {
   	ctx.fillStyle = color;
   	ctx.fillRect(this.x, this.y, this.width, this.height);
 	}
+  this.newPos = function() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+  }
 }
 // Adding movement to my primary object
 function updateGameArea() {
 	gameArea.clear();
+  obstacle.update();
+  myPrimary.newPos();
 	myPrimary.y += 1;
 	myPrimary.update();
 }
 
-//Adding controls
+//Adding control functions
+function stop(){
+  myPrimary.speedY -= 1;
+}
+
+function moveleft(){
+  myPrimary.speedX -= 1;
+}
+
+function moveright(){
+  myPrimary.speedX += 1;
+}
+
+function clearmove(){
+  myPrimary.speedX = 0;
+  myPrimary.speedY = 0;
+}
