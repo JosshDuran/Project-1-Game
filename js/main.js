8e8f8a54ec89acd5732e15f28ff4044c4364b442
Jossh Duran <josshduran@Jennifers-MacBook-Pro.local>
2019-04-02 11:34:31 -0700

    update

--- js/main.js
+++ js/main.js
@@ -4,8 +4,10 @@ document.addEventListener("keydown", movementHandler)
 
 var myPrimary;
 var obstacles = [];
+var scoreBoard;
 
 function startGame() {
+  scoreBoard = new component("20px", "Consolas", "black", 350, 20, "text");
   gameArea.start();
   myPrimary = new component(30, 30, "red", 160, 0);
   // obstacle = new component(10, 200, "purple", 300, 120);
@@ -19,7 +21,7 @@ var gameArea = {
     this.context = this.canvas.getContext("2d");
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
   	this.frameNo = 0;
-    this.interval = setInterval(updateGameArea, 20);
+    this.interval = setInterval(updateGameArea, 8);
   	},
   	clear : function () {
   		this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -34,7 +36,8 @@ function everyinterval(n) {
   return false;
 }
 // Starting Primary object speifications 
-function component(width, height, color, x, y) {
+function component(width, height, color, x, y, type) {
+  this.type = type
   this.width = width;
   this.height = height;
   this.speedX = 0;
@@ -43,8 +46,14 @@ function component(width, height, color, x, y) {
   this.y = y;
   this.update = function() {    
   	ctx = gameArea.context;
+    if (this.type == "text") {
+      ctx.font = this.width + " " + this.height;
+      ctx.fillStyle = color;
+      ctx.fillText(this.text, this.x, this.y);
+    } else {
   	ctx.fillStyle = color;
   	ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
 	}
   this.newPos = function() {
     this.x += this.speedX;
@@ -84,18 +93,20 @@ function updateGameArea() {
     if (gameArea.frameNo == 1 || everyinterval(150)) {
       y = gameArea.canvas.height;
       minWidth = 20;
-      maxWidth = 200;
+      maxWidth = 165;
       width = Math.floor(Math.random()*(maxWidth-minWidth+1)+minWidth);
-      minGap = 50;
-      maxGap = 200;
+      minGap = 160;
+      maxGap = 295;
       gap = Math.floor(Math.random()*(maxGap-minGap+1)+minGap);
-      obstacles.push(new component(width, 10, "purple", 0, y));
-      obstacles.push(new component(y - width - gap, 10, "purple", width + gap,y));
+      obstacles.push(new component(width, 25, "purple", 0, y));
+      obstacles.push(new component(y - width - gap, 25, "purple", width + gap,y));
     }
     for (i = 0; i < obstacles.length; i += 1) {
       obstacles[i].y += -1;
       obstacles[i].update();
     }
+  scoreBoard.text = "SCORE: " + gameArea.frameNo;
+  scoreBoard.update();
   myPrimary.newPos();
   // myPrimary.y += 1;
   myPrimary.update();
@@ -106,16 +117,16 @@ function movementHandler(e) {
 // LEFT=37, UP=38, RIGHT=39, DOWN=40, 
   switch (e.keyCode) {
     case (37):
-      myPrimary.x -= 10
+      myPrimary.x -= 5
     break
     case (38):
-      myPrimary.y -= 10
+      myPrimary.y -= 5
     break
     case (39):
-      myPrimary.x += 10
+      myPrimary.x += 5
     break
     case (40):
-      myPrimary.y += 10
+      myPrimary.y += 5
     break
   }
 }
